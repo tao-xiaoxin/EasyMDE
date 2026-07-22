c43132efebefae25e5ac2f04188c02e8276c6742
hermes-echo <hermes-echo@hotmail.com>
2026-07-22 17:44:35 +0800

    build(editor): refresh production bundle after focus fix

--- assets/build/assets/admin-editor-CppHYq5W.asset.php
+++ assets/build/assets/admin-editor-CppHYq5W.asset.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 	'dependencies' => array( 'media-editor', 'wp-api-fetch', 'wp-element', 'wp-hooks' ),
-	'version'      => '9edf73bea7236355',
+	'version'      => 'da76953ea28e2068',
 );
